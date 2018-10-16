039eabaed61a18e9e705535b5ffedf005189f118
Adam Wittnam <adam.wittnam@hotmail.com>
2018-10-15 17:24:10 -0700

    SeaTac location populating and rendering

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
 
 
-//First Location
+//First Location: 1st and Pike
 function getRandomIntInclusive (minCust, maxCust) {
     return (Math.random() * (maxCust - minCust + 1)) + minCust; //The maximum is inclusive and the minimum is inclusive 
   }
@@ -44,6 +44,45 @@ pike.render = function() {
   }
 
   pike.render();
+
+  //location two:  SeaTac Airport
+
+  function getRandomIntInclusive (minCust, maxCust) {
+    return (Math.random() * (maxCust - minCust + 1)) + minCust; //The maximum is inclusive and the minimum is inclusive 
+  }
+
+  var seaTac = {
+    name: 'SeaTac Airport',
+    minCust: 3,
+    maxCust: 24,
+    avgCookieSale: 1.2,
+    hourlyCookieSales: [],
+    cookiesPerHour:  function() {
+        for (var i = 0; i < hours.length; i++) {
+            this.hourlyCookieSales.push(Math.floor(getRandomIntInclusive(this.minCust, this.maxCust)*this.avgCookieSale));
+           
+        }
+    }
+}
+  
+seaTac.cookiesPerHour();
+  
+
+seaTac.render = function() {
+    
+    var seaTacUl = document.getElementById('SeaTac');
+    
+    for (var i = 0; i < this.hourlyCookieSales.length; i++) {
+      
+      var liEl = document.createElement('li');
+      
+      liEl.textContent = `${hours[i]}: ${this.hourlyCookieSales[i]}`;
+      
+      seaTacUl.appendChild(liEl);
+    }
+  }
+
+  seaTac.render();
 
 
 
