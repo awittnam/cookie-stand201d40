b51d4fc49c57ef3333dc7716bc550587a5b3e16c
Adam Wittnam <adam.wittnam@hotmail.com>
2018-10-15 17:19:09 -0700

    first and pike populates and renders

--- app.js
+++ app.js
@@ -7,21 +7,21 @@ var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'S
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
 
 
+//First Location
 function getRandomIntInclusive (minCust, maxCust) {
     return (Math.random() * (maxCust - minCust + 1)) + minCust; //The maximum is inclusive and the minimum is inclusive 
   }
 
   var pike = {
     name: '1st and Pike',
-    minCust: '23',
-    maxCust: '65',
-    avgCookieSale: '6.3',
+    minCust: 23,
+    maxCust: 65,
+    avgCookieSale: 6.3,
     hourlyCookieSales: [],
     cookiesPerHour:  function() {
         for (var i = 0; i < hours.length; i++) {
             this.hourlyCookieSales.push(Math.floor(getRandomIntInclusive(this.minCust, this.maxCust)*this.avgCookieSale));
-            //console.log(estimatedCust);
-            //var custPerHour = getRandomIntInclusive(minCust, maxCust)avgCookieSale; 
+           
         }
     }
 }
@@ -29,51 +29,30 @@ function getRandomIntInclusive (minCust, maxCust) {
 pike.cookiesPerHour();
   
 
-//pike.estimatedCust = estimatedCust(pike.minCust, pike.maxCust);
+pike.render = function() {
     
+    var pikeUl = document.getElementById('Pike');
+    
+    for (var i = 0; i < this.hourlyCookieSales.length; i++) {
+      
+      var liEl = document.createElement('li');
+      
+      liEl.textContent = `${hours[i]}: ${this.hourlyCookieSales[i]}`;
+      
+      pikeUl.appendChild(liEl);
+    }
+  }
+
+  pike.render();
 
 
 
 
 
-//randomNumGen: function(minCust, maxCust) {
-//     return Math.floor(Math.random() * (this.maxCust - this.minCust)) + this.minCust;
-//   }
-
-// pike.render = function() {
-//     //access  
-//     var pike = document.getElementById('pike');
-//     //for each element inthe seagullCount array, we need to
-//     for (var i = 0; i < alki.seagullCount.length; i++) {
-//         console.log(this.seagullCount[i], 'Alki gulls');
-//     // 1.  create a <li> element
-//     var liEl = document.createElement('li');
-//     // 2.  give that <li> element content 
-//     liEl.textContent = `${days[i]}: ${this.seagullCount[i]}`;
-//     console.log(liEl, 'LiEl');
-//     // 3.  append the <li> to the <ul>
-//     alkiUl.appendChild(liEl); 
-//     }
-// }
-
-// var goldenGardens = {
-//     name: 'Golden Gardens Beach',
-//     neighborhood: 'Ballard',
-//     seagullCount: [12, 42, 33, 18, 2, 34, 17]
-// };
-
-// var goldenGardensUl = document.getElementById('gg');
-
-// var edmonds = {
-//     name: 'Edmonds Beach',
-//     neighborhood: 'Edmonds',
-//     seagullCount: [65, 48, 89, 1, 56, 62, 89]
-// };
-
-// var edmondsUl = document.getElementById('edmonds');
 
 
-// alki.render();
 
-// //goldenGardens.render();
-// //edmonds.render();
+
+
+
+
